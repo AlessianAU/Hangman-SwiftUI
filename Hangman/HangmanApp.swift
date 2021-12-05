e85d602fd1427cebbf59174a32da6b94b6fc5945
AlessianAU <alessia@alessian.org>
2021-12-05 20:13:18 +1100

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Alessia on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cd1114009e384e2e28d673287803c72dd9780ef4
AlessianAU <alessia@alessian.org>
2021-12-16 13:40:56 +1100

    Move entitlements to dedicated folder

--- Hangman/HangmanApp.swift
+++ Hangman/HangmanApp.swift
@@ -2,16 +2,43 @@
 //  HangmanApp.swift
 //  Hangman
 //
-//  Created by Skyla Peters on 12/12/21.
+//  Created by Alessia on 5/12/21.
 //
 
 import SwiftUI
+import Foundation
+#if os(macOS)
+import AppKit
+
 
 @main
 struct HangmanApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+		.windowToolbarStyle(.unified)
+		.windowStyle(HiddenTitleBarWindowStyle())
+	}
 }
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+		return true
+	}
+}
+
+#else
+
+@main
+struct HangmanApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
+
+#endif
